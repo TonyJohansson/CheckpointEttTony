3d094ffb4c8b5242cc4b44ec68c40cb322bb914e
TonyJohansson <tony.johansson.it@outlook.com>
2022-11-09 13:39:13 +0100

    Merge

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hej Tony här!");
         Secondo.banan();
+        Thirdo.kiwi();
 
 
     }
---
+++ src/Thirdo.java
@@ -0,0 +1,8 @@
+public class Thirdo {
+
+    public static void kiwi() {
+
+        System.out.println("Hej Thirdo här");
+    }
+}
+
